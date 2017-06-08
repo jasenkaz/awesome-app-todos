7c4a8caee39c0d5e7254dbeac98f8cc57699c824
jasenkaz <jasenkaz@yahoo.com>
2017-06-08 18:45:58 -0400

    stubbed out basic application structure, mocked a todo, and rendered the mock data structure to our liking

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,31 @@
+const mockTodo = {
+  task: 'get some milk',
+  isComplete: false
+};
 
+const TodoApp = {
+  rootElement: '#app',
+  todos: [mockTodo],
+  start: function(){
+    this.cacheDOM();
+    this.bindEvents();
+    this.render();
+  },
+  cacheDOM: function(){
+    this.root = document.querySelector(this.rootElement);
+    this.addButton = this.root.querySelector('.add-button');
+    this.taskInput = this.root.querySelector('.task-input');
+    this.todoList = this.root.querySelector('.todo-list');
+  },
+  bindEvents: function(){
+    this.addButton.addEventListener('click', () => console.log('add'));
+  },
+  render: function(){
+    const lis = this.todos
+                .map(todo => '<li>' + todo.task + '</li>');
+                .join();
+    this.todoList.innerHTML = lis;
+  }
+};
+
+TodoApp.start();
