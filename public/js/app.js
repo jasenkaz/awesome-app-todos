ce9e6a3caff193e2cdc3c199286dfec7d1261b36
jasenkaz <jasenkaz@yahoo.com>
2017-06-08 19:17:43 -0400

    implemented the add todo method to create lists. cleaned up the mock.

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,7 @@
-const mockTodo = {
-  task: 'get some milk',
-  isComplete: false
-};
 
 const TodoApp = {
   rootElement: '#app',
-  todos: [mockTodo],
+  todos: [],
   start: function(){
     this.cacheDOM();
     this.bindEvents();
@@ -20,14 +16,27 @@ const TodoApp = {
   },
 
   bindEvents: function(){
-    this.addButton.addEventListener('click', () => console.log('add'));
+    this.addButton.addEventListener('click', () => this.addTodo());
   },
-
+addTodo: function(){
+  //grab the task input value, validate that task value is actually something, then build to do object with that value, then add that todo to the todos array, render, clear input
+  const taskValue = this.taskInput.value;
+  const todo = {
+    task: taskValue,
+  if(!taskValue){
+      return;  //if nothing in the box, return makes it stop
+    },
+    isComplete: false
+  };
+  this.todos.push(todo);
+  this.render();
+  this.taskInput.value = '';
+},
   render: function(){
     const lis = this.todos
                 .map(todo => `<li>${todo.task}</li>`)
-                .join();
-    this.todoList.innerHTML = lis;
+                .join('');  //put empty string '' to avoid printing comma in browser
+    this.todoList.innerHTML = lis; //inner html can lead to security problems
   }
 };
 
